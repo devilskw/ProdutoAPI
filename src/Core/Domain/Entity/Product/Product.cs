3bba6e5c17a54e677a6a8f8151ad224c944ddbfd
devilskw <kazuo_w@hotmail.com>
2024-06-04 19:39:26 -0300

    Adding Product Entity

---
+++ src/Core/Domain/Entity/Product/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Domain.Entity.Product;
+public class Product
+{
+  public long id { get; private set; }
+  public string? name { get; private set; }
+
+  public Product(long id, string name)
+  {
+    this.id = id;
+    this.name = name;
+  }
+}
